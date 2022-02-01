167a51c932ad0a0809473db359e8c93db0304f94
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2022-02-01 15:54:36 +0100

    Add URL to THE computer science ranking 2022 in JSON

--- ranking_scraper/ranking_scraper/spiders/the_spider.py
+++ ranking_scraper/ranking_scraper/spiders/the_spider.py
@@ -4,7 +4,8 @@ import scrapy
 class TheSpiderSpider(scrapy.Spider):
     name = 'the_spider'
     allowed_domains = ['timeshighereducation.com']
-    start_urls = ['http://timeshighereducation.com/']
+    start_urls = ['https://www.timeshighereducation.com/sites/default/files/the_data_rankings/computer_science_rankings_2022_0__059c3663af4eb54c21c21270fa703f58.json']
 
     def parse(self, response):
+        print(response.body)
         pass
