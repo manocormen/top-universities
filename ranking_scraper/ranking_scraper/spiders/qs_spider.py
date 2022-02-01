2d856a261f3b7b0f75b28dc1b94031e6b97f3bf4
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2022-02-01 16:46:32 +0100

    Yield each item in QS ranking as a dict

--- ranking_scraper/ranking_scraper/spiders/qs_spider.py
+++ ranking_scraper/ranking_scraper/spiders/qs_spider.py
@@ -4,7 +4,11 @@ import scrapy
 class QsSpiderSpider(scrapy.Spider):
     name = 'qs_spider'
     allowed_domains = ['topuniversities.com']
-    start_urls = ['http://topuniversities.com/']
+    start_urls = ['https://www.topuniversities.com/sites/default/files/qs-rankings-data/en/3516175.txt?1643723564?v=1643729848861']
 
     def parse(self, response):
-        pass
+        data = response.json()  # Convert json to dict
+        ranking_data = data["data"]
+
+        for university in ranking_data:
+            yield university
