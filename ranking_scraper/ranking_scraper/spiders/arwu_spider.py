89d1aaf8a1969a4e28df50e275913d900704aaac
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2022-02-04 08:12:44 +0100

    Move ARWU page parse logic into dedicated method

--- ranking_scraper/ranking_scraper/spiders/arwu_spider.py
+++ ranking_scraper/ranking_scraper/spiders/arwu_spider.py
@@ -17,14 +17,16 @@ class ArwuSpiderSpider(scrapy.Spider):
         page = response.meta["playwright_page"]
         last_page_number = int(await page.locator("li.ant-pagination-item >> nth=-1").inner_text())
 
+        async for item in self.parse_page(page):
+            yield item
+
+        for _ in range(last_page_number - 1):
+            await page.click(".ant-pagination-next")  # Go to next page in ranking
+            async for item in self.parse_page(page):
+                yield item
+    
+    async def parse_page(self, page):
         names = await page.locator("span.univ-name").all_inner_texts()
         ranks = await page.locator("div.ranking").all_inner_texts()
         for name, rank in zip(names, ranks):
             yield {"name": name, "rank": rank}
-
-        for _ in range(last_page_number - 1):
-            await page.click(".ant-pagination-next")
-            names = await page.locator("span.univ-name").all_inner_texts()
-            ranks = await page.locator("div.ranking").all_inner_texts()
-            for name, rank in zip(names, ranks):
-                yield {"name": name, "rank": rank}
